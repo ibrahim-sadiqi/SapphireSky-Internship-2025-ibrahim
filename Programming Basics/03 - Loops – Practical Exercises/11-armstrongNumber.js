9b69e3b7926b936b729dee60a1a9bc1b561e2bcc
Eblali <129132075+Eblali@users.noreply.github.com>
2025-02-19 16:21:40 +0430

    Change comment of arm strong number

--- Programming Basics/03 - Loops – Practical Exercises/11-armstrongNumber.js
+++ Programming Basics/03 - Loops – Practical Exercises/11-armstrongNumber.js
@@ -13,8 +13,8 @@ function isArmstrong(number) {
     const firstNumber = number;
     // change to string and get the length.
     const digitCount = String(firstNumber).length;
-    // Or we can write our own function without change to  
-    // string get the number of digits.
+    // Or, we can write our own function without converting to  
+    // a string to get the number of digits.
     //const digitCount = getDigitsCount(firstNumber);
     let sum = 0;
     while (number > 0) {

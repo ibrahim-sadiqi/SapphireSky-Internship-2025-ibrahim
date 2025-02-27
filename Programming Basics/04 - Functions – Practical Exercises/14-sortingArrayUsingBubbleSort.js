3d460d24bbf1cb8ad18c524e755900ddef6fb707
Eblali <129132075+Eblali@users.noreply.github.com>
2025-02-27 18:32:16 +0430

    Replaced manual swappng with destructuring.

--- Programming Basics/04 - Functions – Practical Exercises/14-sortingArrayUsingBubbleSort.js
+++ Programming Basics/04 - Functions – Practical Exercises/14-sortingArrayUsingBubbleSort.js
@@ -5,9 +5,7 @@ function bubbleSort(arr) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let j = i + 1; j < arr.length; j++) {
       if (arr[i] > arr[j]) {
-        temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+        [arr[i], arr[j]] = [arr[j], arr[i]];
       }
     }
   }

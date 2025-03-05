01548c11a59df2d69ea2b446a85af16997e683ec
Eblali <129132075+Eblali@users.noreply.github.com>
2025-03-05 11:09:36 +0430

    Renamed programming basics folder

--- Programming Basics/02 - If Conditions – Practical Exercises/01-checkOddEven.js
+++
@@ -1,7 +0,0 @@
-let number = Number(prompt('Enter a number:'));
-
-if (number % 2 == 0) {
-    console.log(`${number} is an even number!`);
-} else {
-    console.log(`${number} is an odd number!`);
-}
--- Programming Basics/02 - If Conditions – Practical Exercises/05-triangleValidity.js
+++
@@ -1,9 +0,0 @@
-let a = 10;
-let b = 3;
-let c = 5;
-
-if (a + b > c && a + c > b && b + c > a) {
-    console.log('Valid triangle!')
-} else {
-    console.log('Not valid!')
-}

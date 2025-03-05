ebf53125e117f6342e17478693bb7f10832e4908
Eblali <129132075+Eblali@users.noreply.github.com>
2025-03-05 15:25:08 +0430

    Changed the logic of factorial

--- 01 - Basics of Programming/03 - Loops – Practical Exercises/07-factorial.js
+++ 01 - Basics of Programming/03 - Loops – Practical Exercises/07-factorial.js
@@ -1,10 +1,10 @@
 // Factorial of a Number
 // Implement a function that calculates the factorial of a number using a loop.
 
-const number = 10;
+const number = 1;
 
-let fac = 2;
-for (let i = number; i > 2; i--) {
+let fac = 1;
+for (let i = number; i > 1; i--) {
   fac *= i;
 }
 

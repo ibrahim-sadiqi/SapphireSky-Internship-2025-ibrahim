d87636541a55a7a1463abe4067f0b3c722d16a4c
Eblali <129132075+Eblali@users.noreply.github.com>
2025-03-10 21:58:55 +0430

    Made the reverse algorithm more efficient

--- 01 - Basics of Programming/03 - Loops – Practical Exercises/04-reverseNumber.js
+++ 01 - Basics of Programming/03 - Loops – Practical Exercises/04-reverseNumber.js
@@ -2,11 +2,11 @@
 // Given an integer, reverse its digits. (Example: 1234 → 4321)
 
 let number = 12345;
-let revese = "";
+let revesed = 0;
 while (number > 0) {
-  revese += number % 10;
+  revesed = revesed * 10 + (number % 10);
   number = Math.trunc(number / 10);
 }
 
-const reveseNumber = Number(revese);
+const reveseNumber = revesed;
 console.log(reveseNumber);
